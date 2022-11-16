1329b18ca6ec35f4a0ed48a0ebe26dda5e7e3127
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2022-11-16 03:54:24 +0000

    lab3 part 3 mid Task 5 added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Import the modules we need
-var express = require ('express')
+var express = require('express')
 var ejs = require('ejs')
-var bodyParser= require ('body-parser')
+var bodyParser = require('body-parser')
+const mysql = require('mysql');
 
 // Create the express application object
 const app = express()
@@ -23,10 +24,28 @@ app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
 // Define our data
-var shopData = {shopName: "Bertie's Books"}
+var shopData = { shopName: "Bertie's Books" }
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, shopData);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+// Define the data base connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'appuser',
+    password: 'app2027',
+    database: 'myBookShop'
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
--- routes/main.js
+++ routes/main.js
@@ -1,24 +1,88 @@
-module.exports = function(app, shopData) {
+module.exports = function (app, shopData) {
 
     // Handle our routes
-    app.get('/',function(req,res){
+    app.get('/', function (req, res) {
         res.render('index.ejs', shopData)
     });
-    app.get('/about',function(req,res){
+    app.get('/about', function (req, res) {
         res.render('about.ejs', shopData);
     });
-    app.get('/search',function(req,res){
+
+    app.get('/search', function (req, res) {
         res.render("search.ejs", shopData);
     });
+
     app.get('/search-result', function (req, res) {
         //searching in the database
-        res.send("You searched for: " + req.query.keyword);
+        // res.send("You searched for: " + req.query.keyword);
+        const q = req.query.keyword;
+        // console.log(q);
+        let sqlquery = `SELECT * FROM  books where name LIKE "%${q}%"`;
+        console.log(sqlquery);
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            shopData.result = result;
+            res.render('searchresult.ejs', shopData);
+        });
     });
-    app.get('/register', function (req,res) {
-        res.render('register.ejs', shopData);                                                                     
-    });                                                                                                 
-    app.post('/registered', function (req,res) {
+
+    // regester route
+    app.get('/register', function (req, res) {
+        res.render('register.ejs', shopData);
+    });
+
+    // list route
+    app.get('/list', (req, res) => {
+        let sqlquery = "SELECT * FROM books";
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            res.send(result);
+        });
+    });
+
+    // registered route
+    app.post('/registered', function (req, res) {
         // saving data in database
-        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-    }); 
-}
+        res.send(' Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered!  We will send an email to you at ' + req.body.email);
+    });
+
+
+    // add book  route
+    app.get('/addbook', function (req, res) {
+        res.render("addbook.ejs", shopData);
+    });
+
+    // bookadded route
+    app.post('/bookadded', function (req, res) {
+        // saving data in database
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            else
+                res.send(' This book is added to database, name: ' + req.body.name + ' price ' + req.body.price);
+        });
+    });
+
+    app.get('/bargainbooks', (req, res) => {
+        let sqlquery = "SELECT * FROM books WHERE price > 20";
+        console.log(sqlquery);
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            shopData.result = result;
+            res.render('bargainbooks.ejs', shopData);
+        });
+    });
+
+
+    // end of module
+};
